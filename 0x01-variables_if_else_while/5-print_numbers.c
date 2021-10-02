1bf06ad32e66e3fa4d71346b1ecbc1554c93f58b
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-01 17:48:40 -0700

    print all single digit numbers of base 10 using while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
+
