829cc173c10c9a490f747ed9b391d3873c4c5333
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-01 20:21:59 -0700

    prints all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 including characters
+ * Return: 0
+ */
+int main(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
