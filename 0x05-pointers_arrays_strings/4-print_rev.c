1a33ef2123d44c4c0660be4068c9949d89bd797b
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-12 20:03:23 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len, count;
+
+	len = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+		len++;
+	return (len);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int count, len;
+
+	len = _strlen(s);
+
+	for (count = len; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
