042fd3952fc9533bf48fc04d61ae6f57bf27be9b
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-15 09:19:09 -0700

    reverses the string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,9 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, aux;
 
-	for (i = n - 1 ; i > 0; i--)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[i]);
+	for (i = 0; i < n/2; i++)
+	{
+		aux = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = aux;
+	}     	
 }
