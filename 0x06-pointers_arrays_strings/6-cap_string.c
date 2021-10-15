3a15c22dee353ee7e76f92bbd1a11f50ea0717fa
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-15 08:15:41 -0700

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: pointer to char type
+ *
+ * Return: pointer to str.
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (*(str + i) == ' ' || *(str + i) == '\n' || *(str + i) == '\t'
+		|| *(str + i) == ',' || *(str + i) == ';' || *(str + i) == '.'
+		|| *(str + i) == '!' || *(str + i) == '?' || *(str + i) == '('
+		|| *(str + i) == ')' || *(str + i) == '{' || *(str + i) == '}')
+		{
+			if (*(str + i + 1) >= 'a' && *(str + i + 1) <= 'z')
+				*(str + i + 1) -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
