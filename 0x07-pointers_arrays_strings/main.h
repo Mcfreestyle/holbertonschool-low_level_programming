db5dd9a04c7bbe9c2605ec50718bb399567ca98d
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-18 16:35:14 -0700

    add prototype function_strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 char *_memset(char *s, char, unsigned int);
 char *_memcpy(char *dest, char *src, unsigned int);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
