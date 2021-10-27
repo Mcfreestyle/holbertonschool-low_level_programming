ff5217ea3cbcb943fd291c68fa72d5d0d6f16207
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-26 20:50:01 -0700

    create an array of two dimensions

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create an array of two dimensions
+ * @width: number of columns of the array
+ * @height: number if rows of the string
+ *
+ * Return: pointer to a 2 dimensional array, otherwise NULL
+ *	if the width or height is NULL or there is insufficient space
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, len;
+	int *p, **array;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	len = sizeof(int *) * height + sizeof(int) * width * height;
+	array = malloc(len);
+
+	p = (int *)(array + height);
+
+	for (i = 0; i < height; i++)
+		array[i] = p + width * i;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			*(*(array + i) + j) = 0;
+	}
+	return (array);
+}
