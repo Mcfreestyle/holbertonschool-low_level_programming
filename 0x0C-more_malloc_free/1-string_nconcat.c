40d4282be8a5f681883264ce1895e4c9b12aa29e
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-10-28 14:48:29 -0700

    deleted last added conditional

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,17 +17,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i = 0;
 	j = 0;
+
 	if (s1 == NULL)
 		size1 = 0;
 	else
 		size1 = strlen(s1);
+
 	if (s2 == NULL)
 		size2 = 0;
 	else
 		size2 = strlen(s2);
-
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 
 	if (n < size2)
 	{
@@ -37,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		mem = size1 + size2 + 1;
-		str = malloc(sizeof(str) * mem);
+		str = malloc(sizeof(char) * mem);
 	}
 
 	if (str == NULL)
@@ -45,6 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < size1; i++)
 		*(str + i) = *(s1 + i);
+
 	for (; i < mem - 1; i++)
 	{
 		*(str + i) = *(s2 + j);
