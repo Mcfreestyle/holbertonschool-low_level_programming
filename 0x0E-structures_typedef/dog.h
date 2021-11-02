8aab64baf5c92239dd8aab17f7b213634ab540a1
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-01 19:52:05 -0700

    defines a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - contains the dates of the dog
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
