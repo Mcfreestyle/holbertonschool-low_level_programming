b4bb06fcab5708de3439e1731d9fcc8b7ac2deea
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-01 20:50:45 -0700

    prints the dates of the dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the dates of the dog
+ * @d: pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age != 0)
+			printf("Age: %.6f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
