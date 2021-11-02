fcc014c7f21a054872badce8f734bc82a3b2a9e8
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-02 10:11:05 -0700

    use of macro to reduce code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,7 @@
-#include <string.h>
 #include <stdio.h>
 #include "dog.h"
+
+#define isNull(str) ((str) != NULL ? str : "(nil)")
 
 /**
  * print_dog - prints the dates of the dog
@@ -10,17 +11,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
+		printf("Name: %s\n", isNull(d->name));
 		if (d->age > 0)
 			printf("Age: %.6f\n", d->age);
 		else
-			printf("Age: (nil)\n");
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
+			printf("Age: %.6f\n", .0f);
+		printf("Owner: %s\n", isNull(d->owner));
 	}
 }
