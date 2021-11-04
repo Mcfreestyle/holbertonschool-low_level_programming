5f2f124c3ea1d03a0c9e7bc2c82e716f7243b8c7
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-03 20:45:12 -0700

    different notation to access elements of an array

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,8 +22,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (*s == *(ops + i)->op)
-			return (*(ops + i)->f);
+		if (*s == *(ops[i]).op)
+			return (*(ops[i]).f);
 		i++;
 	}
 	return (*(ops + i)->f);
