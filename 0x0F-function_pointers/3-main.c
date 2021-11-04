0eebf9cb93e6b6e0c2f3c9a5aebe6a80d6549d4f
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-03 19:00:46 -0700

    prints the result of the selected operation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array that contains the values of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int cal, num1, num2;
+	char *operator;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (argc == 4)
+	{
+		if (get_op_func(operator) == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		cal = get_op_func(operator)(num1, num2);
+		printf("%d\n", cal);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
