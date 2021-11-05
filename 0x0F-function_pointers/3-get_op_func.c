2f6e3625b7dde337054eca201f5c87cd42463bc4
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-05 13:04:56 -0700

    conditional to let only one character as operator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (*s == *(ops[i]).op)
+		if (*s == *(ops[i]).op && *(s + 1) == '\0')
 			return (*(ops[i]).f);
 		i++;
 	}
-	return (*(ops + i)->f);
+	return (*(ops[i]).f);
 }
