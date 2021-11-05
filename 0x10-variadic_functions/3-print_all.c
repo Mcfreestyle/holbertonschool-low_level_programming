d28fb5029b71a65cd1c49e389f5a8b64bebda0b5
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-04 21:13:52 -0700

    prints all parameters regardless of data type

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,75 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * match_type - search the character in the string
+ * @list: struct that contains the arguments of the main
+ * @format: string that gives information about the type of the parameters
+ * @i: counter
+ *
+ * Return: 1 if there is match, otherwise 0
+ */
+int match_type(va_list list, const char * const format, int i)
+{
+	char *str;
+
+	switch (*(format + i))
+	{
+		case 'c':
+			{
+				printf("%c", va_arg(list, int));
+				break;
+			}
+		case 'i':
+			{
+				printf("%i", va_arg(list, int));
+				break;
+			}
+		case 'f':
+			{
+				printf("%f", va_arg(list, double));
+				break;
+			}
+		case 's':
+			{
+				str = va_arg(list, char *);
+				if (str != NULL)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+			}
+		default:
+			{
+				return (0);
+			}
+	}
+	return (1);
+}
+
+/**
+ * print_all - prints the parameters(numbers, strings or other)
+ * @format: string that gives information about the type of the arguments
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	unsigned int len, i, j;
+
+	i = 0;
+	len = strlen(format);
+
+	va_start(list, format);
+	while (i < len)
+	{
+		j = match_type(list, format, i);
+		if (i != len - 1 && j == 1)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	putchar('\n');
+}
