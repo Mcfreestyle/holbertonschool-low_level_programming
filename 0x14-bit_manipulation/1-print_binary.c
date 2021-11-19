aa49b5e274746774cee50f40340be2aac1f62b28
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-18 16:40:29 -0800

    fix: use of recursion to comply the requirements

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,12 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
+ * print_binary - prints binary representation of a number
  * @n: number to convert
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int power_two = 1, temp;
-
-	if (n == 0 || n == 1)
-	{
-		_putchar('0' + n);
-		return;
-	}
-	while (power_two < n)
-		power_two *= 2;
-	temp = power_two;
-	while (power_two > 0)
-	{
-		if (power_two <= n)
-		{
-			n -= power_two;
-			_putchar('1');
-		}
-		else if (power_two != temp)
-			_putchar('0');
-		power_two /= 2;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
