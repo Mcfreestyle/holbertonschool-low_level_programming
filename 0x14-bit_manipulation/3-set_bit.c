ba8d38d033d0eb28f5238ece5ca75ede502b9cb7
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-19 16:48:13 -0800

    fix: conditional when index is bigger that the max_bits of the number

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,14 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	unsigned int max_bits;
+	unsigned long int mask = 1;
+
+	max_bits = sizeof(*n) * 8;
+	if (n == NULL || index >= max_bits)
 		return (-1);
-	*n = *n | (1 << index);
+
+	mask = mask << index;
+	*n = *n | mask;
 	return (1);
 }
