862bdd6f108c643894baf39aed3d46f0e93cb80f
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-22 19:54:53 -0800

    docs: creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, nbytes, wr_bytes;
+
+	nbytes = strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	wr_bytes = write(fd, text_content, nbytes);
+	if (wr_bytes < 0)
+		return (-1);
+
+	return (1);
+}
