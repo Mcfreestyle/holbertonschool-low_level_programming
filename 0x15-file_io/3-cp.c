90e20d83a51a35034b8ed5181421f364577e763f
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-23 14:11:46 -0800

    chore: created functions to validate the return of the system calls

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,24 +4,23 @@
 #include <stdlib.h>
 
 /**
- * validation - validates the open system call
- * @ret1: first return
- * @ret2: second return
+ * exit_98 - exits the program because the return of open or read is negative
  * @file_from: source file
+ */
+void exit_98(char *file_from)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
+
+/**
+ * exit_99 - exits the program because the return of open or write is negative
  * @file_to: destiny file
  */
-void validation(int ret1, int ret2, char *file_from, char *file_to)
+void exit_99(char *file_to)
 {
-	if (ret1 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	if (ret2 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
 }
 
 /**
@@ -36,14 +35,24 @@ void copy_content(char *file_from, char *file_to)
 
 
 	fd1 = open(file_from, O_RDONLY);
-	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd1 < 0)
+		exit_98(file_from);
 
-	validation(fd1, fd2, file_from, file_to);
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 < 0)
+		exit_99(file_to);
 
 	for (rbytes = read(fd1, buff, 1024); rbytes; rbytes = read(fd1, buff, 1024))
+	{
+		if (rbytes < 0)
+			exit_98(file_from);
 		wbytes = write(fd2, buff, rbytes);
+		if (wbytes < 0)
+			exit_99(file_to);
+	}
 
-	validation(rbytes, wbytes, file_from, file_to);
+	if (rbytes < 0)
+		exit_98(file_from);
 
 	if (close(fd1) < 0)
 	{
