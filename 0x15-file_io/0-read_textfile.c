c2729b84dd9be8b35c8006eabf80607fcdfae799
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-11-22 19:00:43 -0800

    chore: use of malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -12,19 +13,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, sz;
-	char buff[10000];
+	char *buff;
 
-	if (filename == NULL)
+	buff = malloc(sizeof(char) * letters);
+
+	if (filename == NULL || buff == NULL)
+	{
+		free(buff);
 		return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 
 	sz = read(fd, buff, letters);
+	if (sz < 0)
+		return (0);
 	buff[sz] = '\0';
-	printf("%s", buff);
+	write(STDOUT_FILENO, buff, sz);
 	close(fd);
+	free(buff);
 	return (sz);
 }
 
