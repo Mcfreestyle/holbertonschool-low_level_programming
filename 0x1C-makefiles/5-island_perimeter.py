96d57bce116818cec9c9b1e215dc29ebbbf8739c
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-03-17 17:50:07 -0500

    style: pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,29 +3,28 @@
 
 
 def island_perimeter(grid):
-	"""Calculate the perimeter of the island described in grid
-		Args:
-			grid = list of lists of integers
-		Return:
-			per = perimeter of the island
-	"""
-	per = 0
-	i = 0
-	for row in grid:
-		j = 0
-		for col in row:
-			sides = 4
-			if col == 1:
-				if row[j - 1] == 1:
-					sides -= 1
-				if row[j + 1] == 1:
-					sides -= 1
-				if grid[i - 1][j] == 1:
-					sides -= 1
-				if grid[i + 1][j] == 1:
-					sides -= 1
-				per += sides
-				print(sides)
-			j += 1
-		i += 1
-	return (per)
+    """Calculate the perimeter of the island described in grid
+        Args:
+            grid = list of lists of integers
+        Return:
+            per = perimeter of the island
+    """
+    per = 0
+    i = 0
+    for row in grid:
+        j = 0
+        for col in row:
+            sides = 4
+            if col == 1:
+                if row[j - 1] == 1:
+                    sides -= 1
+                if row[j + 1] == 1:
+                    sides -= 1
+                if grid[i - 1][j] == 1:
+                    sides -= 1
+                if grid[i + 1][j] == 1:
+                    sides -= 1
+                per += sides
+            j += 1
+        i += 1
+    return (per)
